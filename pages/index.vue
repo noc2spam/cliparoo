73e4eb8a79b25ccbe837c42f2555a9cf7767f869
sugata <thcdesigning@gmail.com>
2025-11-03 14:12:30 +0530

    feat: save, delete, copy and code refactorization. Also, data shape is now {title: string, description: string}[]

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,73 @@
 <template>
-    <div>
-        <h1>Clipboard Contents</h1>
-        <ul>
-            <li v-for="(value, key) in clipboard" :key="key">
-                <div>
-                    <strong>{{ key }}:</strong> {{ value }}
-                </div>
-                <div>
-                    <button @click="copyToClipboard(value)">Copy</button>
-                    <button @click="deleteClipboardItem(key as string)">Delete</button>
-                </div>
-            </li>
-        </ul>
-        <button @click="clearClipboard">Clear Clipboard</button>
-        <button @click="addClipboardItem">Add Clipboard Item</button>
-        <div v-if="popupOpen">
-            <h2>Add Clipboard Item</h2>
-            <input v-model="newKey" placeholder="Key" />
-            <input v-model="newValue" placeholder="Value" />
-            <button @click="saveClipboardItem">Save</button>
-            <button @click="popupOpen = false">Cancel</button>
+    <div class="mx-auto p-4 max-w-[600px] w-full">
+        <div class="w-full p-4 bg-gray-100 border border-gray-400 rounded-lg">
+            <h1 class="text-xl font-bold">My Clipboard</h1>
+            <ul v-if="clipboard.length">
+                <li v-for="(pair, key) in clipboard" :key="key">
+                    <div>
+                        <strong>{{ pair.title }}:</strong> {{ pair.description }}
+                    </div>
+                    <div>
+                        <button @click="copyToClipboard(pair.description)">Copy</button>
+                        <button @click="deleteClipboardItem(pair.title as string)">Delete</button>
+                    </div>
+                </li>
+            </ul>
+            <div v-else>
+                <p>Your clipboard is empty.</p>
+            </div>
+            <button @click="clearClipboard">Clear Clipboard</button>
+            <button @click="addClipboardItem">Add Clipboard Item</button>
+            <div v-if="appState.popupOpen">
+                <h2>Add Clipboard Item</h2>
+                <input v-model="title" placeholder="Key" />
+                <input v-model="description" placeholder="Value" />
+                <button @click="saveClipboardItem">Save</button>
+                <button @click="closePopup">Cancel</button>
+            </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">
+
+const appState = useState('appState', () => ({
+    popupOpen: false
+}));
+const clipboard = ref<{ title: string, description: string }[]>([]);
+const title = ref('');
+const description = ref('')
+function addClipboardItem() {
+    Object.assign(appState.value, { popupOpen: true });
+}
 function clearClipboard() {
     localStorage.removeItem('clipboard');
-    for (const key in clipboard) {
-        delete clipboard[key];
-    }
+    Object.assign(clipboard, []);
 }
-const clipboard = reactive<{ [key: string]: string }>({});
-const popupOpen = ref(false);
-const newKey = ref('');
-const newValue = ref('');
-function addClipboardItem() {
-    popupOpen.value = true;
+function closePopup() {
+    Object.assign(appState.value, { popupOpen: false, newKey: '', newValue: '' });
 }
-function saveClipboardItem() {
-    if (newKey.value && newValue.value) {
-        clipboard[newKey.value] = newValue.value;
-        localStorage.setItem('clipboard', JSON.stringify(clipboard));
-        newKey.value = '';
-        newValue.value = '';
-        popupOpen.value = false;
-    }
-}
-function deleteClipboardItem(key: string) {
-    delete clipboard[key];
-    localStorage.setItem('clipboard', JSON.stringify(clipboard));
-}
-
 function copyToClipboard(value: string) {
     navigator.clipboard.writeText(value).then(() => {
         alert('Copied to clipboard!');
     });
 }
+function deleteClipboardItem(key: string) {
+    const currentClipboard = clipboard.value.filter((item) => item.title !== key);
 
+    Object.assign(clipboard, currentClipboard);
+    localStorage.setItem('clipboard', JSON.stringify(currentClipboard));
+}
+function saveClipboardItem() {
+    const currentClipboard = clipboard.value;
+    currentClipboard.push({ title: title.value, description: description.value });
+    Object.assign(clipboard.value, currentClipboard);
+    localStorage.setItem('clipboard', JSON.stringify(currentClipboard));
+    closePopup();
+}
 onMounted(() => {
     const rawClipboard = localStorage.getItem('clipboard');
     if (rawClipboard) {
-        Object.assign(clipboard, JSON.parse(rawClipboard));
+        Object.assign(clipboard.value, JSON.parse(rawClipboard));
     }
 })
 </script>
